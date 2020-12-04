5459493e41be3684b315d18cd10db9bf41602fec
Sidharth-Rava <73884983+Sidharth-Rava@users.noreply.github.com>
2020-12-04 21:32:25 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,30 @@
+class Pig{
+    constructor(x,y){
+        var options ={
+            restitution:0.8,
+            friction : 0.3,
+            density : 1.0
+
+        }
+
+        this.body = Bodies.rectangle(x,y,40,40,options);
+        this.width = 40;
+        this.height = 40; 
+        World.add(world,this.body)   ;
+
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("green")
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+
+    }
+
+}
